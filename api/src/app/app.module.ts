ccc2882c843532b68a2165ef922078819d8fd5b4
Paulo Roberto <paulorantunes13@hotmail.com>
2025-06-20 21:40:27 -0300

    refactor: reorganize core files into app/ folder

---
+++ api/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { CharacterEntity } from 'src/entities/character.entity';
+import { PlanetEntity } from 'src/entities/planet.entity';
+import { SpaceshipEntity } from 'src/entities/spaceship.entity';
+import { StarSystemEntity } from 'src/entities/star-system.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature([
+      CharacterEntity,
+      PlanetEntity,
+      SpaceshipEntity,
+      StarSystemEntity,
+    ]),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
