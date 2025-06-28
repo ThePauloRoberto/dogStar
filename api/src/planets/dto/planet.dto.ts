826422c46a67524d2a646f7ecc678cdcd9affc09
Paulo Roberto <paulorantunes13@hotmail.com>
2025-06-27 21:50:19 -0300

    chore: add validation decorators to PlanetDTO

--- api/src/planets/dto/planet.dto.ts
+++ api/src/planets/dto/planet.dto.ts
@@ -1,7 +1,19 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class PlanetDTO {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  @IsNotEmpty()
+  @IsString()
   climate: string;
+  @IsNotEmpty()
+  @IsString()
   terrain: string;
+  @IsNotEmpty()
+  @IsNumber()
   population: number;
-  starSystem: string;
+  @IsNotEmpty()
+  @IsNumber()
+  starSystem: number;
 }
